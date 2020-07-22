07841b5432f8b2ea52f971f7ebb0a1878f83e93d
cn-src <public@javaer.cn>
2020-07-22 15:01:08 +0800

    update

--- src/test/java/cn/javaer/snippets/jooq/condition/ConditionCreatorTest.java
+++ src/test/java/cn/javaer/snippets/jooq/condition/ConditionCreatorTest.java
@@ -85,6 +85,62 @@ class ConditionCreatorTest {
     }
 
     @Test
+    void treeNodeListToCondition1() throws Exception {
+        //language=JSON
+        final List<TreeNode> treeNodes = this.objectMapper.readValue("[\n" +
+                "  {\n" +
+                "    \"title\": \"t1\"\n" +
+                "  " +
+                "}\n" +
+                "]", new TypeReference<List<TreeNode>>() {});
+        final Condition condition = ConditionCreator.create(treeNodes,
+                DSL.field("f1", String.class));
+        assertThat(this.dsl.renderInlined(condition)).isEqualTo("f1 = 't1'");
+    }
+
+    @Test
+    void treeNodeListToCondition2() throws Exception {
+        //language=JSON
+        final List<TreeNode> treeNodes = this.objectMapper.readValue("[\n" +
+                "  {\n" +
+                "    \"title\": \"t1-1\"\n" +
+                "  " +
+                "}, {\n" +
+                "  \"title\": \"t1-2\"\n" +
+                "}\n" +
+                "]", new TypeReference<List<TreeNode>>() {});
+        final Condition condition = ConditionCreator.create(treeNodes,
+                DSL.field("f1", String.class));
+        assertThat(this.dsl.renderInlined(condition)).isEqualTo("(f1 = 't1-1' or f1 = 't1-2')");
+    }
+
+    @Test
+    void treeNodeListToCondition3() throws Exception {
+        //language=JSON
+        final List<TreeNode> treeNodes = this.objectMapper.readValue("[\n" +
+                "  {\n" +
+                "    \"title\": \"t1-1\",\n" +
+                "    \"children\": [\n" +
+                "      {\n" +
+                "        \"title\": \"t2-1\",\n" +
+                "        \"children\": []\n" +
+                "   " +
+                "   },\n" +
+                "      {\n" +
+                "        \"title\": \"t2-2\",\n" +
+                "        \"children\": []\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  " +
+                "}\n" +
+                "]", new TypeReference<List<TreeNode>>() {});
+        final Condition condition = ConditionCreator.create(treeNodes,
+                DSL.field("f1", String.class),
+                DSL.field("f2", String.class));
+        assertThat(this.dsl.renderInlined(condition)).isEqualTo("((f2 = 't2-1' or f2 = 't2-2') and f1 = 't1-1')");
+    }
+
+    @Test
     void treeNodeListToCondition() throws Exception {
 
         //language=JSON
