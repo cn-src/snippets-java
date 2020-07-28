a95634575cd8faf249e799041babc7859e1ad97c
cn-src <public@javaer.cn>
2020-07-28 17:39:35 +0800

    add JooqJsonbSerializer

---
+++ src/test/java/cn/javaer/snippets/jackson/JooqJsonbSerializerTest.java
@@ -0,0 +1,39 @@
+package cn.javaer.snippets.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.Data;
+import org.jooq.JSONB;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+/**
+ * @author cn-src
+ */
+class JooqJsonbSerializerTest {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp() {
+        final SimpleModule module = new SimpleModule();
+        module.addSerializer(JooqJsonbSerializer.INSTANCE);
+        this.mapper.registerModule(module);
+    }
+
+    @Test
+    void serialize() throws Exception {
+        final Demo demo = new Demo();
+        demo.setStr("val");
+        //language=JSON
+        demo.setJsonb(JSONB.valueOf("{\"demo\":123}"));
+        final String value = this.mapper.writeValueAsString(demo);
+        JSONAssert.assertEquals("{\"str\":\"val\",\"jsonb\":{\"demo\":123}}", value, false);
+    }
+
+    @Data
+    static class Demo {
+        String str;
+        JSONB jsonb;
+    }
+}
