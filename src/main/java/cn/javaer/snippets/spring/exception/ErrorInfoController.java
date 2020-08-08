4c2d64221a5662f41c44680417d6914c1e9ade9b
cn-src <public@javaer.cn>
2020-08-08 21:08:12 +0800

    update

--- src/main/java/cn/javaer/snippets/spring/exception/ErrorInfoController.java
+++ src/main/java/cn/javaer/snippets/spring/exception/ErrorInfoController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeSet;
 
 /**
  * @author cn-src
@@ -13,14 +16,22 @@ import java.util.Map;
 @RequestMapping
 public class ErrorInfoController {
 
-    private final ErrorInfoExtractor errorInfoExtractor;
+    private final Map<String, DefinedErrorInfo> errorInfos;
 
     public ErrorInfoController(final ErrorInfoExtractor errorInfoExtractor) {
-        this.errorInfoExtractor = errorInfoExtractor;
+
+        final TreeSet<Map.Entry<String, DefinedErrorInfo>> objects =
+                new TreeSet<>(Comparator.comparing(it -> it.getValue().getStatus()));
+        objects.addAll(errorInfoExtractor.getErrorInfos().entrySet());
+
+        this.errorInfos = new LinkedHashMap<>(objects.size());
+        for (final Map.Entry<String, DefinedErrorInfo> entry : objects) {
+            this.errorInfos.put(entry.getKey(), entry.getValue());
+        }
     }
 
     @GetMapping("error_infos")
     public Map<String, DefinedErrorInfo> errorInfos() {
-        return this.errorInfoExtractor.getErrorInfos();
+        return this.errorInfos;
     }
 }
