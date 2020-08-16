1d7be657117aafc2cc92ae9efa6f579eb448925c
cn-src <public@javaer.cn>
2020-08-16 15:20:32 +0800

    update

--- src/main/java/cn/javaer/snippets/spring/autoconfigure/jackson/JooqJacksonAutoConfiguration.java
+++ src/main/java/cn/javaer/snippets/spring/autoconfigure/jackson/JooqJacksonAutoConfiguration.java
@@ -2,13 +2,12 @@ package cn.javaer.snippets.spring.autoconfigure.jackson;
 
 import cn.javaer.snippets.jackson.JooqJsonbSerializer;
 import org.jooq.JSONB;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.jackson.JsonComponentModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -16,20 +15,12 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass({JSONB.class})
-@ConditionalOnBean({JsonComponentModule.class})
-@AutoConfigureAfter(JacksonAutoConfiguration.class)
+@AutoConfigureBefore(JacksonAutoConfiguration.class)
 @ConditionalOnProperty(prefix = "snippets.jackson", name = "enabled", havingValue = "true",
         matchIfMissing = true)
-public class JooqJacksonAutoConfiguration implements InitializingBean {
-    private final JsonComponentModule jsonComponentModule;
-
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public JooqJacksonAutoConfiguration(final JsonComponentModule jsonComponentModule) {
-        this.jsonComponentModule = jsonComponentModule;
-    }
-
-    @Override
-    public void afterPropertiesSet() {
-        this.jsonComponentModule.addSerializer(JooqJsonbSerializer.INSTANCE);
+public class JooqJacksonAutoConfiguration {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer snippetsJacksonCustomizer() {
+        return it -> it.serializerByType(JSONB.class, JooqJsonbSerializer.INSTANCE);
     }
 }
--- src/main/java/cn/javaer/snippets/spring/autoconfigure/jackson/SnippetsJacksonAutoConfiguration.java
+++ src/main/java/cn/javaer/snippets/spring/autoconfigure/jackson/SnippetsJacksonAutoConfiguration.java
@@ -9,14 +9,13 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jackson.JsonComponentModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -30,49 +29,39 @@ import java.time.format.DateTimeFormatter;
  * @author cn-src
  */
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnBean({ObjectMapper.class, JsonComponentModule.class})
-@AutoConfigureAfter(JacksonAutoConfiguration.class)
+@ConditionalOnClass({ObjectMapper.class})
+@AutoConfigureBefore(JacksonAutoConfiguration.class)
 @ConditionalOnProperty(prefix = "snippets.jackson", name = "enabled", havingValue = "true",
         matchIfMissing = true)
 @EnableConfigurationProperties(SnippetsJacksonProperties.class)
-public class SnippetsJacksonAutoConfiguration implements InitializingBean {
-    private final ObjectMapper objectMapper;
-    private final JsonComponentModule jsonComponentModule;
-    private final SnippetsJacksonProperties snippetsJacksonProperties;
+public class SnippetsJacksonAutoConfiguration {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public SnippetsJacksonAutoConfiguration(final ObjectMapper objectMapper,
-                                            final JsonComponentModule jsonComponentModule,
-                                            final SnippetsJacksonProperties snippetsJacksonProperties) {
-        this.objectMapper = objectMapper;
-        this.jsonComponentModule = jsonComponentModule;
-        this.snippetsJacksonProperties = snippetsJacksonProperties;
-    }
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer snippetsJacksonCustomizer(final SnippetsJacksonProperties snippetsJacksonProperties) {
+        return it -> {
+            it.annotationIntrospector(SnippetsJacksonIntrospector.INSTANCE);
 
-    @Override
-    public void afterPropertiesSet() {
-        this.objectMapper.setAnnotationIntrospector(SnippetsJacksonIntrospector.INSTANCE);
+            final DateTimeFormatter dateTimeFormatter =
+                    DateTimeFormatter.ofPattern(snippetsJacksonProperties.getFormat().getDateTime());
+            final DateTimeFormatter dateFormatter =
+                    DateTimeFormatter.ofPattern(snippetsJacksonProperties.getFormat().getDate());
+            final DateTimeFormatter timeFormatter =
+                    DateTimeFormatter.ofPattern(snippetsJacksonProperties.getFormat().getTime());
 
-        final DateTimeFormatter dateTimeFormatter =
-                DateTimeFormatter.ofPattern(this.snippetsJacksonProperties.getFormat().getDateTime());
-        final DateTimeFormatter dateFormatter =
-                DateTimeFormatter.ofPattern(this.snippetsJacksonProperties.getFormat().getDate());
-        final DateTimeFormatter timeFormatter =
-                DateTimeFormatter.ofPattern(this.snippetsJacksonProperties.getFormat().getTime());
+            it.deserializerByType(LocalDateTime.class,
+                    new LocalDateTimeDeserializer(dateTimeFormatter));
+            it.deserializerByType(LocalDate.class,
+                    new LocalDateDeserializer(dateFormatter));
+            it.deserializerByType(LocalTime.class,
+                    new LocalTimeDeserializer(timeFormatter));
 
-        this.jsonComponentModule.addDeserializer(LocalDateTime.class,
-                new LocalDateTimeDeserializer(dateTimeFormatter));
-        this.jsonComponentModule.addDeserializer(LocalDate.class,
-                new LocalDateDeserializer(dateFormatter));
-        this.jsonComponentModule.addDeserializer(LocalTime.class,
-                new LocalTimeDeserializer(timeFormatter));
-
-        this.jsonComponentModule.addSerializer(LocalDateTime.class,
-                new LocalDateTimeSerializer(dateTimeFormatter));
-        this.jsonComponentModule.addSerializer(LocalDate.class,
-                new LocalDateSerializer(dateFormatter));
-        this.jsonComponentModule.addSerializer(LocalTime.class,
-                new LocalTimeSerializer(timeFormatter));
+            it.serializerByType(LocalDateTime.class,
+                    new LocalDateTimeSerializer(dateTimeFormatter));
+            it.serializerByType(LocalDate.class,
+                    new LocalDateSerializer(dateFormatter));
+            it.serializerByType(LocalTime.class,
+                    new LocalTimeSerializer(timeFormatter));
+        };
     }
 
     @Bean
