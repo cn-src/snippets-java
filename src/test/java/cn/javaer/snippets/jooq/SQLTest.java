8118280b71db7d326f4d3a8fde371c8d03d0ff46
cn-src <public@javaer.cn>
2020-08-18 17:55:40 +0800

    update

--- src/test/java/cn/javaer/snippets/jooq/SQLTest.java
+++ src/test/java/cn/javaer/snippets/jooq/SQLTest.java
@@ -42,4 +42,14 @@ class SQLTest {
         assertThat(this.dsl.renderInlined(jsonbField))
                 .isEqualTo("jsonb_object_agg(f1,f2)");
     }
+
+    @Test
+    void jsonbObjectAgg2() {
+        final JsonbField<Record, JSONB> jsonbField = SQL.jsonbObjectAgg(new Field[]{DSL.field("f1"
+                , String.class), DSL.field(
+                "f2", String.class)}, '-', DSL.field(
+                "v1", String.class));
+        assertThat(this.dsl.renderInlined(jsonbField))
+                .isEqualTo("jsonb_object_agg((f1 || '-' || f2),v1)");
+    }
 }
