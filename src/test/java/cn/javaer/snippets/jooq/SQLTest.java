5f1034816abd05140f299e7648496cbfe5774cf3
cn-src <public@javaer.cn>
2020-08-18 16:29:20 +0800

    update

--- src/test/java/cn/javaer/snippets/jooq/SQLTest.java
+++ src/test/java/cn/javaer/snippets/jooq/SQLTest.java
@@ -1,7 +1,10 @@
 package cn.javaer.snippets.jooq;
 
+import cn.javaer.snippets.jooq.field.JsonbField;
 import org.jooq.DSLContext;
 import org.jooq.Field;
+import org.jooq.JSONB;
+import org.jooq.Record;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
 import org.junit.jupiter.api.Test;
@@ -18,15 +21,25 @@ class SQLTest {
     @Test
     void stContains() {
 
-        Field<Boolean> stContains = SQL.stContains(DSL.field("geom_a", SQL.GEOMETRY_TYPE), DSL.field("geom_b", SQL.GEOMETRY_TYPE));
+        final Field<Boolean> stContains = SQL.stContains(DSL.field("geom_a", SQL.GEOMETRY_TYPE),
+                DSL.field("geom_b", SQL.GEOMETRY_TYPE));
         assertThat(this.dsl.renderInlined(stContains))
                 .isEqualTo("ST_Contains(geom_a, geom_b)");
     }
 
     @Test
     void stAsGeoJSON() {
-        Field<String> stAsGeoJSON = SQL.stAsGeoJSON(DSL.field("geom", SQL.GEOMETRY_TYPE));
+        final Field<String> stAsGeoJSON = SQL.stAsGeoJSON(DSL.field("geom", SQL.GEOMETRY_TYPE));
         assertThat(this.dsl.renderInlined(stAsGeoJSON))
                 .isEqualTo("ST_AsGeoJSON(geom)");
+    }
+
+    @Test
+    void jsonbObjectAgg() {
+        final JsonbField<Record, JSONB> jsonbField = SQL.jsonbObjectAgg(DSL.field("f1"
+                , String.class), DSL.field(
+                "f2", String.class));
+        assertThat(this.dsl.renderInlined(jsonbField))
+                .isEqualTo("jsonb_object_agg(f1,f2)");
     }
 }
