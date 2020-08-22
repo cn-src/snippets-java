85e8cba36a7f099c70d79ec5b56e7f54aa238923
cn-src <public@javaer.cn>
2020-08-22 10:44:37 +0800

    update

---
+++ src/main/java/cn/javaer/snippets/spring/web/ResponseUtils.java
@@ -0,0 +1,66 @@
+package cn.javaer.snippets.spring.web;
+
+import cn.javaer.snippets.util.GzipUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+/**
+ * @author cn-src
+ */
+public interface ResponseUtils {
+
+    /**
+     * GZIP 响应.
+     *
+     * @param body body
+     *
+     * @return bytes
+     */
+    static ResponseEntity<byte[]> gzip(@NonNull final String body) {
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.CONTENT_ENCODING, "gzip")
+                .body(GzipUtils.zip(body));
+    }
+
+    /**
+     * 资源响应，文件下载.
+     *
+     * @param resource 资源
+     *
+     * @return 资源
+     */
+    static ResponseEntity<Resource> resource(@NonNull final Resource resource) {
+        return resource(resource, Objects.requireNonNull(resource.getFilename()));
+    }
+
+    /**
+     * 资源响应，文件下载.
+     *
+     * @param resource 资源
+     * @param filename 文件名
+     *
+     * @return 资源
+     */
+    static ResponseEntity<Resource> resource(@NonNull final Resource resource,
+                                             @NonNull final String filename) {
+        Assert.notNull(resource, () -> "Resource must be not null");
+        Assert.notNull(filename, () -> "Filename must be not null");
+
+        final String file = new String(filename.getBytes(StandardCharsets.UTF_8),
+                StandardCharsets.ISO_8859_1);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + file + "\"")
+                .body(resource);
+    }
+}
