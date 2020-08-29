2214a8ef41440fb0d810ae3a68a576b5c7597884
cn-src <public@javaer.cn>
2020-08-29 16:32:15 +0800

    update

--- src/test/java/cn/javaer/snippets/model/TreeNodeTest.java
+++ src/test/java/cn/javaer/snippets/model/TreeNodeTest.java
@@ -1,5 +1,6 @@
 package cn.javaer.snippets.model;
 
+import cn.javaer.snippets.jackson.Json;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,5 +20,31 @@ class TreeNodeTest {
         assertThat(t1.getChildren()).hasSize(2)
             .extracting(TreeNode::getTitle)
             .contains("t1_1", "t1_2");
+    }
+
+    @Test
+    void json() {
+        //language=JSON
+        final TreeNode node1 = Json.INSTANCE.read("{\"title\": \"t1\"}", TreeNode.class);
+        assertThat(node1).extracting(TreeNode::getTitle).isEqualTo("t1");
+    }
+
+    @Test
+    void json2() {
+        //language=JSON
+        final TreeNode node2 = Json.INSTANCE.read("{\"title\": \"t1\",\"children\":[{\"title\": " +
+            "\"t2\"}]}", TreeNode.class);
+        assertThat(node2).extracting(TreeNode::getTitle).isEqualTo("t1");
+        assertThat(node2.getChildren()).hasSize(1).extracting(TreeNode::getTitle).contains("t2");
+    }
+
+    @Test
+    void json3() {
+        //language=JSON
+        final TreeNode node3 = Json.INSTANCE.read("{\"title\": \"t1\",\"children\":[{\"title\": " +
+            "\"t2\"}],\"m1\": 1}", TreeNode.class);
+        assertThat(node3).extracting(TreeNode::getTitle).isEqualTo("t1");
+        assertThat(node3.getChildren()).hasSize(1).extracting(TreeNode::getTitle).contains("t2");
+        assertThat(node3.getDynamic()).containsEntry("m1", 1);
     }
 }
