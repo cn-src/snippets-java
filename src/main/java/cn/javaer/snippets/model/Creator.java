5ef45db3685d2caabc24b0e1ff52a7dcdfff894d
cn-src <public@javaer.cn>
2020-09-05 11:23:49 +0800

    update

---
+++ src/main/java/cn/javaer/snippets/model/Creator.java
@@ -0,0 +1,25 @@
+package cn.javaer.snippets.model;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import lombok.Value;
+
+/**
+ * 适用于需要展示创建者信息，不支持反序列化.
+ *
+ * @author cn-src
+ */
+@Value
+public class Creator<T, C> {
+    @JsonUnwrapped
+    T unwrapped;
+    C createdBy;
+
+    Creator(final T unwrapped, final C createdBy) {
+        this.unwrapped = unwrapped;
+        this.createdBy = createdBy;
+    }
+
+    public static <T, C> Creator<T, C> of(final T unwrapped, final C createdBy) {
+        return new Creator<>(unwrapped, createdBy);
+    }
+}
