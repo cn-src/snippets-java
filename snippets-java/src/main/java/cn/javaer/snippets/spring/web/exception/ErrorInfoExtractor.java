b2d8d95cab7518a466b8492b57f0b95dba133d84
cn-src <public@javaer.cn>
2020-09-17 17:21:21 +0800

    update

--- snippets-java/src/main/java/cn/javaer/snippets/spring/web/exception/ErrorInfoExtractor.java
+++ snippets-java/src/main/java/cn/javaer/snippets/spring/web/exception/ErrorInfoExtractor.java
@@ -1,35 +1,20 @@
 package cn.javaer.snippets.spring.web.exception;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.ConversionNotSupportedException;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.validation.BindException;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.annotation.PostConstruct;
 import java.lang.reflect.Method;
@@ -58,9 +43,9 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
     private ApplicationContext applicationContext;
 
     public ErrorInfoExtractor(
-            final Map<String, DefinedErrorInfo> errorMapping,
-            final ResourceBundleMessageSource messageSource,
-            final boolean isIncludeMessage) {
+        final Map<String, DefinedErrorInfo> errorMapping,
+        final ResourceBundleMessageSource messageSource,
+        final boolean isIncludeMessage) {
 
         this.messageSource = messageSource;
         this.messageSourceAccessor = new MessageSourceAccessor(messageSource, Locale.CHINESE);
@@ -74,21 +59,21 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
         this.initErrorMapping();
         for (final String error : this.errors) {
             Arrays.stream(HttpStatus.values())
-                    .filter(it -> it.name().equals(error))
-                    .findFirst()
-                    .ifPresent(it -> {
-                        if (this.isIncludeMessage) {
-                            final String message = this.messageSourceAccessor.getMessage(
-                                    error, "No message available");
-                            this.errorInfos.put(error, new DefinedErrorInfo(it, message));
-                        }
-                        else {
-                            this.errorInfos.put(error, new DefinedErrorInfo(it));
-                        }
-                    });
+                .filter(it -> it.name().equals(error))
+                .findFirst()
+                .ifPresent(it -> {
+                    if (this.isIncludeMessage) {
+                        final String message = this.messageSourceAccessor.getMessage(
+                            error, "No message available");
+                        this.errorInfos.put(error, new DefinedErrorInfo(it, message));
+                    }
+                    else {
+                        this.errorInfos.put(error, new DefinedErrorInfo(it));
+                    }
+                });
         }
         final Collection<Object> controllers =
-                this.applicationContext.getBeansWithAnnotation(Controller.class).values();
+            this.applicationContext.getBeansWithAnnotation(Controller.class).values();
 
         final Set<Class<? extends Throwable>> throwables = this.findThrowables(controllers);
         for (final Class<? extends Throwable> throwable : throwables) {
@@ -97,17 +82,17 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
                 final String error = extracted.getError();
                 if (this.errors.contains(error)) {
                     if (this.errorInfos.containsKey(error) &&
-                            !this.errorInfos.get(error).getStatus().equals(extracted.getStatus())) {
+                        !this.errorInfos.get(error).getStatus().equals(extracted.getStatus())) {
                         final String msg = String.format("Error:'%s', Has multiple status: '%d' " +
-                                        "and '%d'",
-                                error, this.errorInfos.get(error).getStatus(),
-                                extracted.getStatus());
+                                "and '%d'",
+                            error, this.errorInfos.get(error).getStatus(),
+                            extracted.getStatus());
 
                         throw new IllegalStateException(msg);
                     }
                     if (this.isIncludeMessage) {
                         final String message = this.messageSourceAccessor.getMessage(
-                                error, "No message available");
+                            error, "No message available");
                         this.errorInfos.put(error, extracted.withMessage(message));
                     }
                     else {
@@ -119,53 +104,63 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
     }
 
     void initErrorMapping() {
-        this.internalErrorMapping.put(HttpRequestMethodNotSupportedException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.METHOD_NOT_ALLOWED));
+        this.internalErrorMapping.put(
+            "org.springframework.web.HttpRequestMethodNotSupportedException",
+            DefinedErrorInfo.of(HttpStatus.METHOD_NOT_ALLOWED)
+        );
 
-        this.internalErrorMapping.put(HttpMediaTypeNotSupportedException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.UNSUPPORTED_MEDIA_TYPE));
+        this.internalErrorMapping.put(
+            "org.springframework.web.HttpMediaTypeNotSupportedException",
+            DefinedErrorInfo.of(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+        );
 
-        this.internalErrorMapping.put(HttpMediaTypeNotAcceptableException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.NOT_ACCEPTABLE));
+        this.internalErrorMapping.put(
+            "org.springframework.web.HttpMediaTypeNotAcceptableException",
+            DefinedErrorInfo.of(HttpStatus.NOT_ACCEPTABLE)
+        );
 
-        this.internalErrorMapping.put(MissingPathVariableException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.web.bind.MissingPathVariableException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(MissingServletRequestParameterException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.web.bind.MissingServletRequestParameterException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(ServletRequestBindingException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.web.bind.ServletRequestBindingException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(ConversionNotSupportedException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR));
+        this.internalErrorMapping.put(
+            "org.springframework.beans.TypeMismatchException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(TypeMismatchException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.http.converter.HttpMessageNotReadableException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(HttpMessageNotReadableException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.web.bind.MethodArgumentNotValidException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(HttpMessageNotWritableException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR));
+        this.internalErrorMapping.put(
+            "org.springframework.web.multipart.support.MissingServletRequestPartException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(MethodArgumentNotValidException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.validation.BindException",
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
-        this.internalErrorMapping.put(MissingServletRequestPartException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+        this.internalErrorMapping.put(
+            "org.springframework.web.servlet.NoHandlerFoundException",
+            DefinedErrorInfo.of(HttpStatus.NOT_FOUND));
 
-        this.internalErrorMapping.put(BindException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
-
-        this.internalErrorMapping.put(NoHandlerFoundException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.NOT_FOUND));
-
-        this.internalErrorMapping.put(AsyncRequestTimeoutException.class.getName(),
-                new DefinedErrorInfo(HttpStatus.SERVICE_UNAVAILABLE));
+        this.internalErrorMapping.put(
+            "org.springframework.web.context.request.async.AsyncRequestTimeoutException",
+            DefinedErrorInfo.of(HttpStatus.SERVICE_UNAVAILABLE));
 
         this.internalErrorMapping.put("javax.validation.ConstraintViolationException",
-                new DefinedErrorInfo(HttpStatus.BAD_REQUEST));
+            DefinedErrorInfo.of(HttpStatus.BAD_REQUEST));
 
         if (!CollectionUtils.isEmpty(this.errorMapping)) {
             this.internalErrorMapping.putAll(this.errorMapping);
@@ -174,22 +169,22 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
 
     void initErrors() {
         final Method getResourceBundle =
-                Objects.requireNonNull(ReflectionUtils.findMethod(this.messageSource.getClass(),
-                        "getResourceBundle", String.class, Locale.class));
+            Objects.requireNonNull(ReflectionUtils.findMethod(this.messageSource.getClass(),
+                "getResourceBundle", String.class, Locale.class));
         getResourceBundle.setAccessible(true);
         final Method getDefaultLocale =
-                Objects.requireNonNull(ReflectionUtils.findMethod(this.messageSourceAccessor.getClass(),
-                        "getDefaultLocale"));
+            Objects.requireNonNull(ReflectionUtils.findMethod(this.messageSourceAccessor.getClass(),
+                "getDefaultLocale"));
         getDefaultLocale.setAccessible(true);
         final Locale locale = (Locale) ReflectionUtils.invokeMethod(getDefaultLocale,
-                this.messageSourceAccessor);
+            this.messageSourceAccessor);
 
         for (final String basename : this.messageSource.getBasenameSet()) {
 
             final ResourceBundle resourceBundle =
-                    (ResourceBundle) ReflectionUtils.invokeMethod(getResourceBundle,
-                            this.messageSource,
-                            basename, locale);
+                (ResourceBundle) ReflectionUtils.invokeMethod(getResourceBundle,
+                    this.messageSource,
+                    basename, locale);
             if (null != resourceBundle && !resourceBundle.keySet().isEmpty()) {
                 this.errors.addAll(resourceBundle.keySet());
             }
@@ -224,7 +219,7 @@ public class ErrorInfoExtractor implements ApplicationContextAware {
         }
 
         final ResponseStatus responseStatus = AnnotationUtils.findAnnotation(clazz,
-                ResponseStatus.class);
+            ResponseStatus.class);
         if (null != responseStatus) {
             final HttpStatus httpStatus = responseStatus.code();
             String error = responseStatus.reason();
