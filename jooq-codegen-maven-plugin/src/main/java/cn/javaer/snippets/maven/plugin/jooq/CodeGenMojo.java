1b35ac14d94e52055da66068f2673fe0181f5c10
cn-src <public@javaer.cn>
2020-09-17 10:55:36 +0800

    update

--- jooq-codegen-maven-plugin/src/main/java/cn/javaer/snippets/maven/plugin/jooq/CodeGenMojo.java
+++ jooq-codegen-maven-plugin/src/main/java/cn/javaer/snippets/maven/plugin/jooq/CodeGenMojo.java
@@ -7,6 +7,9 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -29,6 +32,11 @@ public class CodeGenMojo extends AbstractMojo {
     private MavenProject project;
 
     @Parameter(
+        property = "jooq.codegen.skip"
+    )
+    private boolean skip;
+
+    @Parameter(
         property = "jooq.codegen.basedir"
     )
     private String basedir;
@@ -44,12 +52,48 @@ public class CodeGenMojo extends AbstractMojo {
     private List<String> includePackages;
 
     @Override
-    public void execute()
-        throws MojoExecutionException {
-        final String actualBasedir = this.basedir == null ?
-            Paths.get(this.project.getBasedir().getAbsolutePath(), "src/main/java").toString() :
-            this.basedir;
-        CodeGenTool.generate(actualBasedir, this.packageName,
-            this.includePackages.toArray(new String[0]));
+    public void execute() throws MojoExecutionException {
+        if (this.skip) {
+            this.getLog().info("Skipping jOOQ code generation");
+            return;
+        }
+        final ClassLoader oldLoader = Thread.currentThread().getContextClassLoader();
+        final URLClassLoader pluginClassLoader = this.getClassLoader();
+        try {
+            Thread.currentThread().setContextClassLoader(pluginClassLoader);
+            final String actualBasedir = this.basedir == null ?
+                Paths.get(this.project.getBasedir().getAbsolutePath(), "src/main/java").toString() :
+                this.basedir;
+            CodeGenTool.generate(actualBasedir, this.packageName,
+                this.includePackages.toArray(new String[0]));
+        }
+        catch (final Exception ex) {
+            throw new MojoExecutionException("Error running jOOQ code generation tool", ex);
+        }
+        finally {
+            Thread.currentThread().setContextClassLoader(oldLoader);
+            try {
+                pluginClassLoader.close();
+            }
+            catch (final Throwable e) {
+                this.getLog().error("Couldn't close the classloader.", e);
+            }
+        }
+    }
+
+    private URLClassLoader getClassLoader() throws MojoExecutionException {
+        try {
+            final List<String> classpathElements = this.project.getRuntimeClasspathElements();
+            final URL[] urls = new URL[classpathElements.size()];
+
+            for (int i = 0; i < urls.length; i++) {
+                urls[i] = new File(classpathElements.get(i)).toURI().toURL();
+            }
+
+            return new URLClassLoader(urls, this.getClass().getClassLoader());
+        }
+        catch (final Exception e) {
+            throw new MojoExecutionException("Couldn't create a classloader.", e);
+        }
     }
 }
