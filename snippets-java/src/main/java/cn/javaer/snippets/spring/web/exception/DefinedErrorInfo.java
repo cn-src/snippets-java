e6dc78f6f8adfc73d0f64b0865b302d494050f72
cn-src <public@javaer.cn>
2020-09-17 17:12:30 +0800

    update

--- snippets-java/src/main/java/cn/javaer/snippets/spring/web/exception/DefinedErrorInfo.java
+++ snippets-java/src/main/java/cn/javaer/snippets/spring/web/exception/DefinedErrorInfo.java
@@ -13,25 +13,47 @@ public class DefinedErrorInfo {
     Integer status;
     @With String message;
 
+    @Deprecated
     public DefinedErrorInfo(final String error, final Integer status, final String message) {
         this.error = error;
         this.status = status;
         this.message = message;
     }
 
+    @Deprecated
     public DefinedErrorInfo(final String error, final Integer status) {
         this.error = error;
         this.status = status;
         this.message = "";
     }
 
+    @Deprecated
     public DefinedErrorInfo(final HttpStatus httpStatus) {
         this(httpStatus.name(), httpStatus.value(), httpStatus.getReasonPhrase());
     }
 
+    @Deprecated
     public DefinedErrorInfo(final HttpStatus httpStatus, final String message) {
         this.error = httpStatus.name();
         this.status = httpStatus.value();
         this.message = message;
+    }
+
+    public static DefinedErrorInfo of(final String error, final Integer status,
+                                      final String message) {
+        return new DefinedErrorInfo(error, status, message);
+    }
+
+    public static DefinedErrorInfo of(final String error, final Integer status) {
+        return new DefinedErrorInfo(error, status, "");
+    }
+
+    public static DefinedErrorInfo of(final HttpStatus httpStatus) {
+        return new DefinedErrorInfo(httpStatus.name(), httpStatus.value(),
+            httpStatus.getReasonPhrase());
+    }
+
+    public static DefinedErrorInfo of(final HttpStatus httpStatus, final String message) {
+        return new DefinedErrorInfo(httpStatus.name(), httpStatus.value(), message);
     }
 }
