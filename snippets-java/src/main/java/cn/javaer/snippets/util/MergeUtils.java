2e2c2ae75dcc04553e9a0d8ff645eb6b5abe22f5
cn-src <public@javaer.cn>
2020-10-14 11:04:58 +0800

    update

--- snippets-java/src/main/java/cn/javaer/snippets/util/MergeUtils.java
+++ snippets-java/src/main/java/cn/javaer/snippets/util/MergeUtils.java
@@ -6,12 +6,12 @@ import cn.javaer.snippets.model.Creator;
 import cn.javaer.snippets.model.DynamicAssembler;
 import cn.javaer.snippets.util.function.Consumer3;
 import cn.javaer.snippets.util.function.Function3;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
@@ -26,7 +26,7 @@ public interface MergeUtils {
 
     static <S, R> List<R> merge(final List<S> sList,
                                 final BiPredicate<S, R> mergePredicate,
-                                final BiConsumer<S, @Nullable R> handler,
+                                final BiConsumer<S, R> handler,
                                 final Function<S, R> rCreator) {
 
         Objects.requireNonNull(mergePredicate);
@@ -53,7 +53,7 @@ public interface MergeUtils {
 
     static <S> List<S> merge(final List<S> sList,
                              final BiPredicate<S, S> mergePredicate,
-                             final BiConsumer<S, @Nullable S> handler) {
+                             final BiConsumer<S, S> handler) {
         return merge(sList, mergePredicate, handler, s -> s);
     }
 
@@ -72,7 +72,7 @@ public interface MergeUtils {
      */
     static <S, P, R> List<R> merge(final List<S> sList, final List<P> pList,
                                    final BiPredicate<S, P> mergePredicate,
-                                   final BiFunction<S, @Nullable P, R> resultFun) {
+                                   final BiFunction<S, Optional<P>, R> resultFun) {
 
         Objects.requireNonNull(mergePredicate);
         Objects.requireNonNull(resultFun);
@@ -82,11 +82,11 @@ public interface MergeUtils {
 
         final List<R> result = new ArrayList<>(sList.size());
         for (final S s : sList) {
-            P used = null;
+            Optional<P> used = Optional.empty();
             if (null != pList) {
                 for (final P p : pList) {
                     if (mergePredicate.test(s, p)) {
-                        used = p;
+                        used = Optional.of(p);
                         break;
                     }
                 }
@@ -110,7 +110,7 @@ public interface MergeUtils {
      */
     static <S, P> List<S> merge(final List<S> sList, final List<P> pList,
                                 final BiPredicate<S, P> mergePredicate,
-                                final BiConsumer<S, @Nullable P> resultFun) {
+                                final BiConsumer<S, Optional<P>> resultFun) {
         return merge(sList, pList, mergePredicate, (s, p) -> {
             resultFun.accept(s, p);
             return s;
@@ -120,7 +120,7 @@ public interface MergeUtils {
     static <S, P, R> List<R> merge(final List<S> sList, final List<P> pList,
                                    final BiPredicate<S, P> mergePredicate1,
                                    final BiPredicate<S, P> mergePredicate2,
-                                   final Function3<S, @Nullable P, @Nullable P, R> resultFun) {
+                                   final Function3<S, Optional<P>, Optional<P>, R> resultFun) {
 
         Objects.requireNonNull(mergePredicate1);
         Objects.requireNonNull(mergePredicate2);
@@ -131,15 +131,15 @@ public interface MergeUtils {
 
         final List<R> result = new ArrayList<>(sList.size());
         for (final S s : sList) {
-            P p1 = null;
-            P p2 = null;
+            Optional<P> p1 = Optional.empty();
+            Optional<P> p2 = Optional.empty();
             if (pList != null) {
                 for (final P p : pList) {
                     if (mergePredicate1.test(s, p)) {
-                        p1 = p;
+                        p1 = Optional.of(p);
                     }
                     if (mergePredicate2.test(s, p)) {
-                        p2 = p;
+                        p2 = Optional.of(p);
                         break;
                     }
                 }
@@ -152,7 +152,7 @@ public interface MergeUtils {
     static <S, P> List<S> merge(final List<S> sList, final List<P> pList,
                                 final BiPredicate<S, P> mergePredicate1,
                                 final BiPredicate<S, P> mergePredicate2,
-                                final Consumer3<S, @Nullable P, @Nullable P> resultFun) {
+                                final Consumer3<S, Optional<P>, Optional<P>> resultFun) {
         return merge(sList, pList, mergePredicate1, mergePredicate2, (s, p1, p2) -> {
             resultFun.accept(s, p1, p2);
             return s;
@@ -163,7 +163,7 @@ public interface MergeUtils {
         final List<S> sList,
         final List<P1> p1List, final BiPredicate<S, P1> mergePredicate1,
         final List<P2> p2List, final BiPredicate<S, P2> mergePredicate2,
-        final Function3<S, P1, P2, R> resultFun) {
+        final Function3<S, Optional<P1>, Optional<P2>, R> resultFun) {
 
         Objects.requireNonNull(mergePredicate1);
         Objects.requireNonNull(mergePredicate2);
@@ -172,33 +172,33 @@ public interface MergeUtils {
             return Collections.emptyList();
         }
 
-        final Function<S, P1> getP1;
+        final Function<S, Optional<P1>> getP1;
         if (p1List == null || p1List.isEmpty()) {
-            getP1 = (s) -> null;
+            getP1 = (s) -> Optional.empty();
         }
         else {
             getP1 = (s) -> {
                 for (final P1 p : p1List) {
                     if (mergePredicate1.test(s, p)) {
-                        return p;
+                        return Optional.of(p);
                     }
                 }
-                return null;
+                return Optional.empty();
             };
         }
 
-        final Function<S, P2> getP2;
+        final Function<S, Optional<P2>> getP2;
         if (p2List == null || p2List.isEmpty()) {
-            getP2 = (s) -> null;
+            getP2 = (s) -> Optional.empty();
         }
         else {
             getP2 = (s) -> {
                 for (final P2 p : p2List) {
                     if (mergePredicate2.test(s, p)) {
-                        return p;
+                        return Optional.of(p);
                     }
                 }
-                return null;
+                return Optional.empty();
             };
         }
         final List<R> result = new ArrayList<>(sList.size());
@@ -212,7 +212,7 @@ public interface MergeUtils {
         final List<S> sList,
         final List<P1> p1List, final BiPredicate<S, P1> mergePredicate1,
         final List<P2> p2List, final BiPredicate<S, P2> mergePredicate2,
-        final Consumer3<S, P1, P2> resultFun) {
+        final Consumer3<S, Optional<P1>, Optional<P2>> resultFun) {
         return merge(sList, p1List, mergePredicate1, p2List, mergePredicate2, (s, p1, p2) -> {
             resultFun.accept(s, p1, p2);
             return s;
@@ -249,8 +249,8 @@ public interface MergeUtils {
         final List<S> sList, final List<P> pList,
         final BiPredicate<S, P> mergePredicate) {
 
-        return merge(sList, pList, mergePredicate, (s, p) -> {
-            return Assemblers.ofDynamic(s, propName, p);
+        return merge(sList, pList, mergePredicate, (s, opt) -> {
+            return Assemblers.ofDynamic(s, propName, opt.orElse(null));
         });
     }
 
@@ -266,7 +266,9 @@ public interface MergeUtils {
     static <S, P> List<Creator<S, P>> mergeToCreator(
         final List<S> sList, final List<P> pList,
         final BiPredicate<S, P> mergePredicate) {
-        return merge(sList, pList, mergePredicate, (BiFunction<S, P, Creator<S, P>>) Creator::of);
+        return merge(sList, pList, mergePredicate, (s, p) -> {
+            return Creator.of(s, p.orElse(null));
+        });
     }
 
     static <S, P> List<Auditor<S, P>> mergeToAuditor(
@@ -274,6 +276,8 @@ public interface MergeUtils {
         final BiPredicate<S, P> mergePredicate1,
         final BiPredicate<S, P> mergePredicate2) {
         return merge(sList, pList, mergePredicate1, mergePredicate2,
-            (Function3<S, P, P, Auditor<S, P>>) Auditor::of);
+            (s, updatedOpt, createdOpt) -> {
+                return Auditor.of(s, updatedOpt.orElse(null), createdOpt.orElse(null));
+            });
     }
 }
