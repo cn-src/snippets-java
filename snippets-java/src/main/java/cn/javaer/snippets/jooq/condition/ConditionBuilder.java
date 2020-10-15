d9280b596c7ae9bd4fce72bcaa180dec755ecd22
cn-src <public@javaer.cn>
2020-10-15 16:25:36 +0800

    update

--- snippets-java/src/main/java/cn/javaer/snippets/jooq/condition/ConditionBuilder.java
+++ snippets-java/src/main/java/cn/javaer/snippets/jooq/condition/ConditionBuilder.java
@@ -1,12 +1,11 @@
 package cn.javaer.snippets.jooq.condition;
 
 import cn.javaer.snippets.model.TreeNode;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jooq.Condition;
 import org.jooq.Field;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.Nullable;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -35,48 +34,55 @@ public class ConditionBuilder {
         return this;
     }
 
-    public ConditionBuilder appendNotNull(final Field<?>... fields) {
-        if (null != fields) {
-            Arrays.stream(fields).forEach(field -> this.conditions.add(field.isNotNull()));
+    public ConditionBuilder isNotNull(final Field<?>... fields) {
+        if (null != fields && fields.length > 0) {
+            if (fields.length == 1) {
+                this.conditions.add(fields[0].isNotNull());
+            }
+            else {
+                Arrays.stream(fields).forEach(field -> this.conditions.add(field.isNotNull()));
+            }
         }
         return this;
     }
 
-    public ConditionBuilder append(final Supplier<Condition> supplier) {
-        this.conditions.add(supplier.get());
+    public ConditionBuilder append(@NotNull final Supplier<@Nullable Condition> supplier) {
+        this.append(supplier.get());
+        return this;
+    }
+
+    public ConditionBuilder required(@NotNull final Supplier<@NotNull Condition> supplier) {
+        this.conditions.add(Objects.requireNonNull(supplier.get()));
         return this;
     }
 
     @SafeVarargs
     public final ConditionBuilder append(final List<TreeNode> treeNodes,
-                                         @NonNull final Field<String>... fields) {
+                                         @NotNull final Field<String>... fields) {
         return this.append(ConditionCreator.create(treeNodes, fields));
     }
 
-    public <T> ConditionBuilder append(final Function<T, Condition> fun, final T value) {
-        final T obj = this.filter(value);
-        if (ObjectUtils.isEmpty(obj)) {
+    public <T> ConditionBuilder append(@NotNull final Function<T, Condition> fun, final T value) {
+        if (ObjectUtils.isEmpty(value)) {
             return this;
         }
 
-        this.conditions.add(fun.apply(obj));
+        this.conditions.add(fun.apply(value));
         return this;
     }
 
-    public <T1, T2> ConditionBuilder append(final BiFunction<T1, T2, Condition> fun,
+    public <T1, T2> ConditionBuilder append(@NotNull final BiFunction<T1, T2, Condition> fun,
                                             final T1 t1, final T2 t2) {
-        final T1 obj1 = this.filter(t1);
-        final T2 obj2 = this.filter(t2);
-        if (ObjectUtils.isEmpty(obj1) || ObjectUtils.isEmpty(obj2)) {
+        if (ObjectUtils.isEmpty(t1) || ObjectUtils.isEmpty(t2)) {
             return this;
         }
-        this.conditions.add(fun.apply(obj1, obj2));
+        this.conditions.add(fun.apply(t1, t2));
         return this;
     }
 
-    public ConditionBuilder dateTime(final DataTimeBiFunction fun,
+    public ConditionBuilder dateTime(@NotNull final DataTimeBiFunction fun,
                                      final LocalDate start, final LocalDate end) {
-        if (null == start || null == end) {
+        if (null == start && null == end) {
             return this;
         }
         final LocalDateTime startTime = Objects.requireNonNull(start).atTime(LocalTime.MIN);
@@ -85,43 +91,22 @@ public class ConditionBuilder {
         return this;
     }
 
-    public ConditionBuilder dateTime(final DataTimeBiFunction fun,
+    public ConditionBuilder dateTime(@NotNull final DataTimeBiFunction fun,
                                      final LocalDateTime start, final LocalDateTime end) {
-        if (null == start || null == end) {
+        if (null == start && null == end) {
             return this;
         }
         this.conditions.add(fun.apply(start, end));
         return this;
     }
 
-    public <T1, T2, T3> ConditionBuilder append(final Function3<T1, T2, T3> fun,
+    public <T1, T2, T3> ConditionBuilder append(@NotNull final Function3<T1, T2, T3> fun,
                                                 final T1 t1, final T2 t2, final T3 t3) {
-        final T1 obj1 = this.filter(t1);
-        final T2 obj2 = this.filter(t2);
-        final T3 obj3 = this.filter(t3);
-        if (ObjectUtils.isEmpty(obj1) || ObjectUtils.isEmpty(obj2) || ObjectUtils.isEmpty(obj3)) {
+        if (ObjectUtils.isEmpty(t1) || ObjectUtils.isEmpty(t2) || ObjectUtils.isEmpty(t3)) {
             return this;
         }
-        this.conditions.add(fun.apply(obj1, obj2, obj3));
+        this.conditions.add(fun.apply(t1, t2, t3));
         return this;
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> T filter(final T t) {
-        if (null == t) {
-            return null;
-        }
-        if (t instanceof String[]) {
-            return (T) Arrays.stream((String[]) t)
-                .filter(StringUtils::hasLength)
-                .toArray(String[]::new);
-        }
-        if (t instanceof Object[]) {
-            return (T) Arrays.stream((Object[]) t)
-                .filter(Objects::nonNull)
-                .toArray();
-        }
-        return t;
     }
 
     @Nullable
