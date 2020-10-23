2cb980e59496c91ab3ea30ef382cdad4bcfe5fcd
cn-src <public@javaer.cn>
2020-10-23 09:40:14 +0800

    update

--- snippets-java/src/test/java/cn/javaer/snippets/kryo/City.java
+++ snippets-java/src/test/java/cn/javaer/snippets/kryo/City.java
@@ -2,10 +2,8 @@ package cn.javaer.snippets.kryo;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
 public class City {
 
     @Id
