2f626c918db2d3b6b16d8ea37a1a56718bd9a2d7
cn-src <public@javaer.cn>
2020-11-11 16:39:54 +0800

    update

--- snippets-java/src/main/java/cn/javaer/snippets/jooq/codegen/withentity/ExcludeDeclared.java
+++ snippets-java/src/main/java/cn/javaer/snippets/jooq/codegen/withentity/ExcludeDeclared.java
@@ -13,6 +13,6 @@ import java.lang.annotation.Target;
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
+@Target({ElementType.FIELD})
 public @interface ExcludeDeclared {
 }
