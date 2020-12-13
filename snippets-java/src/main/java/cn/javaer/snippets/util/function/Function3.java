eb7fc33bfda0c5311f8d2dd293052e68d505eaf1
cn-src <public@javaer.cn>
2020-12-14 00:13:54 +0800

    其它: 修复警告

--- snippets-java/src/main/java/cn/javaer/snippets/util/function/Function3.java
+++ snippets-java/src/main/java/cn/javaer/snippets/util/function/Function3.java
@@ -6,5 +6,14 @@ package cn.javaer.snippets.util.function;
 @FunctionalInterface
 public interface Function3<T1, T2, T3, R> {
 
+    /**
+     * 有返回值，有3个参数的函数.
+     *
+     * @param t1 the t1
+     * @param t2 the t2
+     * @param t3 the t3
+     *
+     * @return the r
+     */
     R apply(T1 t1, T2 t2, T3 t3);
 }
