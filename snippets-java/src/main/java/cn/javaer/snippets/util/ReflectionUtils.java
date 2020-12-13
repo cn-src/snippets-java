db4efb96dacdac2a85f84705bdb84ece9deff4d0
cn-src <public@javaer.cn>
2020-12-14 00:11:45 +0800

    其它: 修复警告

--- snippets-java/src/main/java/cn/javaer/snippets/util/ReflectionUtils.java
+++ snippets-java/src/main/java/cn/javaer/snippets/util/ReflectionUtils.java
@@ -7,6 +7,13 @@ import java.util.Optional;
  */
 public interface ReflectionUtils {
 
+    /**
+     * 判断 class 是否存在.
+     *
+     * @param className the class name
+     *
+     * @return 返回 true 如果存在
+     */
     static boolean isPresent(final String className) {
         try {
             Class.forName(className);
@@ -17,6 +24,13 @@ public interface ReflectionUtils {
         }
     }
 
+    /**
+     * 获取 class.
+     *
+     * @param className the class name
+     *
+     * @return the class
+     */
     static Optional<Class<?>> getClass(final String className) {
         try {
             return Optional.of(Class.forName(className));
