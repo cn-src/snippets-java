8a47fda00b12f47ff4e51cb5784ff28eb4f9b94d
cn-src <public@javaer.cn>
2020-12-14 00:07:18 +0800

    其它: 修复警告

--- snippets-java/src/main/java/cn/javaer/snippets/model/TreeNodeHandler.java
+++ snippets-java/src/main/java/cn/javaer/snippets/model/TreeNodeHandler.java
@@ -7,5 +7,13 @@ public interface TreeNodeHandler<T> {
 
     @SuppressWarnings("rawtypes") TreeNodeHandler EMPTY = (treeNode, t, depth, index) -> { };
 
+    /**
+     * TreeNode 自定义处理.
+     *
+     * @param treeNode TreeNode
+     * @param t 模型的范型
+     * @param depth 深度
+     * @param index 索引
+     */
     void apply(TreeNode treeNode, T t, int depth, int index);
 }
