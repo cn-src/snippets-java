48d99555aa8019b96a75b0962a63cb9d0eefe62a
cn-src <public@javaer.cn>
2020-12-16 23:39:02 +0800

    特性: CrudStep 开发

--- snippets-java/src/main/java/cn/javaer/snippets/jooq/FieldsProvider.java
+++ snippets-java/src/main/java/cn/javaer/snippets/jooq/FieldsProvider.java
@@ -2,9 +2,52 @@ package cn.javaer.snippets.jooq;
 
 import org.jooq.Field;
 
+import java.time.LocalDateTime;
+
 /**
  * @author cn-src
  */
-public interface FieldsProvider {
-    Field<?>[] declaredFields();
+public interface FieldsProvider<ID> {
+
+    /**
+     * id 主键字段.
+     *
+     * @return Field
+     */
+    Field<ID> idField();
+
+    /**
+     * crud 默认使用的字段.
+     *
+     * @return Field
+     */
+    Field<?>[] crudFields();
+
+    /**
+     * 创建者字段.
+     *
+     * @return Field
+     */
+    Field<Object> createdByField();
+
+    /**
+     * 创建时间字段.
+     *
+     * @return Field
+     */
+    Field<LocalDateTime> createdDateField();
+
+    /**
+     * 更新者字段.
+     *
+     * @return Field
+     */
+    Field<Object> updatedByField();
+
+    /**
+     * 更新时间字段.
+     *
+     * @return Field
+     */
+    Field<LocalDateTime> updatedDateField();
 }
