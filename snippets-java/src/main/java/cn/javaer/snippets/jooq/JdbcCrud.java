40b5a750768649b48d44f8ca38220c9037ac92a2
cn-src <public@javaer.cn>
2020-12-18 21:44:09 +0800

    特性: CrudStep 开发

--- snippets-java/src/main/java/cn/javaer/snippets/jooq/JdbcCrud.java
+++ snippets-java/src/main/java/cn/javaer/snippets/jooq/JdbcCrud.java
@@ -1,5 +1,8 @@
 package cn.javaer.snippets.jooq;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.Validate;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -39,11 +42,44 @@ public class JdbcCrud {
         return this.crudStep.findAllByCreatorStep(meta).fetchInto(clazz);
     }
 
-    public <T> void insert(final T entity) {
+    public <T> int insert(final T entity) {
         Objects.requireNonNull(entity);
 
         @SuppressWarnings("unchecked") final Class<T> clazz = (Class<T>) entity.getClass();
         final TableMetaProvider<T> tableMeta = CrudReflection.getTableMeta(clazz);
-        this.crudStep.insertStep(entity, tableMeta).execute();
+        return this.crudStep.insertStep(entity, tableMeta).execute();
+    }
+
+    public <T> int batchInsert(final List<T> entities) {
+        Validate.notEmpty(entities);
+
+        @SuppressWarnings("unchecked") final Class<T> clazz = (Class<T>) entities.get(0).getClass();
+        final TableMetaProvider<T> tableMeta = CrudReflection.getTableMeta(clazz);
+        return this.crudStep.batchInsertStep(entities, tableMeta).execute();
+    }
+
+    public <T> int update(final T entity) {
+        Objects.requireNonNull(entity);
+
+        @SuppressWarnings("unchecked") final Class<T> clazz = (Class<T>) entity.getClass();
+        final TableMetaProvider<T> meta = CrudReflection.getTableMeta(clazz);
+        return this.crudStep.dynamicUpdateStep(entity, meta, o -> true).execute();
+    }
+
+    public <T> int dynamicUpdate(final T entity) {
+        Objects.requireNonNull(entity);
+
+        @SuppressWarnings("unchecked") final Class<T> clazz = (Class<T>) entity.getClass();
+        final TableMetaProvider<T> meta = CrudReflection.getTableMeta(clazz);
+        return this.crudStep.dynamicUpdateStep(entity, meta, ObjectUtils::isNotEmpty).execute();
+    }
+
+    public <T> int dynamicUpdateByCreator(final T entity) {
+        Objects.requireNonNull(entity);
+
+        @SuppressWarnings("unchecked") final Class<T> clazz = (Class<T>) entity.getClass();
+        final TableMetaProvider<T> meta = CrudReflection.getTableMeta(clazz);
+        return this.crudStep.dynamicUpdateByCreatorStep(entity, meta, ObjectUtils::isNotEmpty)
+            .execute();
     }
 }
