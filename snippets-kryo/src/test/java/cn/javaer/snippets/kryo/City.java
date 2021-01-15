214b947bd81e62b6126a98c39265cf6d950f3b03
cn-src <public@javaer.cn>
2021-01-15 11:38:42 +0800

    重构: 重新划分模块

--- snippets-kryo/src/test/java/cn/javaer/snippets/kryo/City.java
+++ snippets-kryo/src/test/java/cn/javaer/snippets/kryo/City.java
@@ -1,12 +1,10 @@
 package cn.javaer.snippets.kryo;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Data
 public class City {
 
-    @Id
     private Long id;
 
     private String name;
