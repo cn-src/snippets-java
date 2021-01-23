6d4df0bbd8fa8c4bef7454ab3713cc6ea0946a5a
cn-src <public@javaer.cn>
2021-01-23 14:48:31 +0800

    优化: 任务执行器的创建

--- snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/task/SnippetsTaskExecutionAutoConfiguration.java
+++ snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/task/SnippetsTaskExecutionAutoConfiguration.java
@@ -2,6 +2,7 @@ package cn.javaer.snippets.spring.autoconfigure.task;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.task.TaskExecutionProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -25,17 +26,16 @@ public class SnippetsTaskExecutionAutoConfiguration implements ApplicationContex
     @Override
     public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
         final SnippetsTaskExecutionProperties properties =
-                applicationContext.getBean(SnippetsTaskExecutionProperties.class);
+            applicationContext.getBean(SnippetsTaskExecutionProperties.class);
         final DefaultListableBeanFactory beanFactory =
-                (DefaultListableBeanFactory) applicationContext
-                        .getAutowireCapableBeanFactory();
+            (DefaultListableBeanFactory) applicationContext
+                .getAutowireCapableBeanFactory();
 
         if (!CollectionUtils.isEmpty(properties.getExecutions())) {
-
             for (final Map.Entry<String, SnippetsTaskExecutionProperties.TaskProperties> entry :
-                    properties.getExecutions().entrySet()) {
+                properties.getExecutions().entrySet()) {
                 final SnippetsTaskExecutionProperties.TaskProperties taskProp =
-                        entry.getValue();
+                    entry.getValue();
                 final TaskExecutionProperties.Pool pool = taskProp.getPool();
                 TaskExecutorBuilder builder = new TaskExecutorBuilder();
                 builder = builder.queueCapacity(pool.getQueueCapacity());
@@ -43,15 +43,19 @@ public class SnippetsTaskExecutionAutoConfiguration implements ApplicationContex
                 builder = builder.maxPoolSize(pool.getMaxSize());
                 builder = builder.allowCoreThreadTimeOut(pool.isAllowCoreThreadTimeout());
                 builder = builder.keepAlive(pool.getKeepAlive());
+
                 final TaskExecutionProperties.Shutdown shutdown = taskProp.getShutdown();
                 builder = builder.awaitTermination(shutdown.isAwaitTermination());
-                builder =
-                        builder.awaitTerminationPeriod(shutdown
-                                .getAwaitTerminationPeriod());
+                builder = builder.awaitTerminationPeriod(shutdown.getAwaitTerminationPeriod());
                 builder = builder.threadNamePrefix(taskProp.getThreadNamePrefix());
-
                 builder = builder.taskDecorator(taskProp.getTaskDecorator());
-                beanFactory.registerSingleton(entry.getKey(), builder.build());
+
+                final GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+                beanDefinition.setBeanClass(ThreadPoolTaskExecutor.class);
+                beanDefinition.setLazyInit(true);
+                final TaskExecutorBuilder fb = builder;
+                beanDefinition.setInstanceSupplier(fb::build);
+                beanFactory.registerBeanDefinition(entry.getKey(), beanDefinition);
             }
         }
     }
