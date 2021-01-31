a33d0609de295bfe11b2297d0bb54de43c27064c
cn-src <public@javaer.cn>
2021-01-31 11:42:17 +0800

    特性: p6spy 日期字符串替换为字符串转日期函数

---
+++ snippets-p6spy/src/main/java/cn/javaer/snippets/p6spy/DelegateValue.java
@@ -0,0 +1,47 @@
+package cn.javaer.snippets.p6spy;
+
+import com.p6spy.engine.common.Value;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author cn-src
+ */
+class DelegateValue extends Value {
+    private final String url;
+    private static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    public DelegateValue(final Value value, final String url) {
+        super(value.getValue());
+        this.url = url;
+    }
+
+    @SuppressWarnings("AlibabaUndefineMagicConstant")
+    @Override
+    public String convertToString(final Object value) {
+        if (value instanceof Timestamp) {
+            if (this.url == null) {
+                return super.convertToString(value);
+            }
+            if (this.url.contains(":h2:")) {
+                final String val = new SimpleDateFormat(PATTERN).format(value);
+                return String.format("parsedatetime('%s', 'yyyy-MM-dd HH:mm:ss')", val);
+            }
+            if (this.url.contains(":postgresql:")
+                || this.url.contains(":pgsql:")) {
+                final String val = new SimpleDateFormat(PATTERN).format(value);
+                return String.format("to_timestamp('%s', 'yyyy-mm-dd hh24:mi:ss')", val);
+            }
+            if (this.url.contains(":mysql:") || this.url.contains(":mariadb:")) {
+                final String val = new SimpleDateFormat(PATTERN).format(value);
+                return "str_to_date('" + val + "','%Y-%m-%d %H:%i:%s')";
+            }
+            if (this.url.contains(":oracle:")) {
+                final String val = new SimpleDateFormat(PATTERN).format(value);
+                return "to_date('" + val + "','yyyy-mm-dd hh24:mi:ss')";
+            }
+        }
+        return super.convertToString(value);
+    }
+}
