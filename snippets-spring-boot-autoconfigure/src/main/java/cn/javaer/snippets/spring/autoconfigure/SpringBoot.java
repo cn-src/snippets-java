e26d52ad8517232ff2ffa57f6aab24d5ca5bcece
cn-src <public@javaer.cn>
2021-02-03 23:30:28 +0800

    重构: SpringBoot

--- snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/SpringBoot.java
+++ snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/SpringBoot.java
@@ -1,12 +1,10 @@
 package cn.javaer.snippets.spring.autoconfigure;
 
+import cn.javaer.snippets.util.IoUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.UncheckedIOException;
-import java.util.Properties;
 
 /**
  * @author cn-src
@@ -15,15 +13,8 @@ public class SpringBoot {
     public static ConfigurableApplicationContext run(final Class<?> primarySource,
                                                      final String... args) {
         final SpringApplication app = new SpringApplication(primarySource);
-        final Properties props = new Properties();
-        try (final InputStream in =
-                 SpringBoot.class.getResourceAsStream("/default-boot.properties")) {
-            props.load(in);
-        }
-        catch (final IOException e) {
-            throw new UncheckedIOException(e);
-        }
-        app.setDefaultProperties(props);
+        final InputStream input = SpringBoot.class.getResourceAsStream("/default-boot.properties");
+        app.setDefaultProperties(IoUtils.readProperties(input));
         return app.run(args);
     }
 }
