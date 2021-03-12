8cc2694b7c88d04e81886143a9436916bf901747
cn-src <public@javaer.cn>
2021-03-12 12:44:24 +0800

    重构: 重构统一异常处理

--- snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/web/exception/ExceptionAutoConfiguration.java
+++ snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/autoconfigure/web/exception/ExceptionAutoConfiguration.java
@@ -6,16 +6,20 @@ import cn.javaer.snippets.spring.web.exception.ErrorInfoExtractor;
 import cn.javaer.snippets.spring.web.exception.GlobalErrorAttributes;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.servlet.View;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -67,8 +71,15 @@ public class ExceptionAutoConfiguration implements InitializingBean {
     }
 
     @Bean
+    @ConditionalOnMissingBean(value = ErrorAttributes.class, search = SearchStrategy.CURRENT)
     GlobalErrorAttributes globalErrorAttributes(final ErrorInfoExtractor errorInfoExtractor) {
         return new GlobalErrorAttributes(errorInfoExtractor);
+    }
+
+    @Bean(name = "error")
+    @ConditionalOnMissingBean(name = "error")
+    View errorView() {
+        return new ErrorView();
     }
 
     @Override
