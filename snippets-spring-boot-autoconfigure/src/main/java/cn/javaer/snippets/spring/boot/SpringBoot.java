f8d02efb866a666f86b3fb72cb89355a1bb1ff28
cn-src <public@javaer.cn>
2021-04-20 15:49:15 +0800

    重构: 移动类

--- snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/boot/SpringBoot.java
+++ snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/boot/SpringBoot.java
@@ -1,29 +1,20 @@
-package cn.javaer.snippets.spring.autoconfigure;
+package cn.javaer.snippets.spring.boot;
 
 import cn.javaer.snippets.util.IoUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.InputStream;
-import java.util.Properties;
 
 /**
  * @author cn-src
  */
-public class SpringBootAdminLocal {
+public class SpringBoot {
     public static ConfigurableApplicationContext run(final Class<?> primarySource,
                                                      final String... args) {
         final SpringApplication app = new SpringApplication(primarySource);
-
         final InputStream input = SpringBoot.class.getResourceAsStream("/default-boot.properties");
-        final Properties props = IoUtils.readProperties(input);
-
-        final InputStream adminInput = SpringBoot.class
-            .getResourceAsStream("/default-boot-admin-local.properties");
-        final Properties adminProps = IoUtils.readProperties(adminInput);
-        props.putAll(adminProps);
-        
-        app.setDefaultProperties(props);
+        app.setDefaultProperties(IoUtils.readProperties(input));
         return app.run(args);
     }
 }
--- snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/boot/SpringBootAdminLocal.java
+++ snippets-spring-boot-autoconfigure/src/main/java/cn/javaer/snippets/spring/boot/SpringBootAdminLocal.java
@@ -1,20 +1,29 @@
-package cn.javaer.snippets.spring.autoconfigure;
+package cn.javaer.snippets.spring.boot;
 
 import cn.javaer.snippets.util.IoUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * @author cn-src
  */
-public class SpringBoot {
+public class SpringBootAdminLocal {
     public static ConfigurableApplicationContext run(final Class<?> primarySource,
                                                      final String... args) {
         final SpringApplication app = new SpringApplication(primarySource);
+
         final InputStream input = SpringBoot.class.getResourceAsStream("/default-boot.properties");
-        app.setDefaultProperties(IoUtils.readProperties(input));
+        final Properties props = IoUtils.readProperties(input);
+
+        final InputStream adminInput = SpringBoot.class
+            .getResourceAsStream("/default-boot-admin-local.properties");
+        final Properties adminProps = IoUtils.readProperties(adminInput);
+        props.putAll(adminProps);
+
+        app.setDefaultProperties(props);
         return app.run(args);
     }
 }
