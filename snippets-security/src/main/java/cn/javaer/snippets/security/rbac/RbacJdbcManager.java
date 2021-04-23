e44f4acbbff8ad4fabef510c255dd6414dbc3cc7
cn-src <public@javaer.cn>
2021-04-23 22:14:34 +0800

    特性: 开发 snippets-security

--- snippets-security/src/main/java/cn/javaer/snippets/security/rbac/RbacJdbcManager.java
+++ snippets-security/src/main/java/cn/javaer/snippets/security/rbac/RbacJdbcManager.java
@@ -7,11 +7,16 @@ import org.jooq.impl.DSL;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
+import static cn.javaer.snippets.security.rbac.gen.TPermission.PERMISSION;
+import static cn.javaer.snippets.security.rbac.gen.TPermission.PERMISSION_FIELDS;
+import static cn.javaer.snippets.security.rbac.gen.TPermissionDetails.PERMISSION_DETAILS;
 import static cn.javaer.snippets.security.rbac.gen.TRole.ROLE;
 import static cn.javaer.snippets.security.rbac.gen.TRoleDetails.ROLE_DETAILS;
 import static cn.javaer.snippets.security.rbac.gen.TRolePermission.ROLE_PERMISSION;
+import static cn.javaer.snippets.security.rbac.gen.TUserPermission.USER_PERMISSION;
 
 /**
  * @author cn-src
@@ -25,34 +30,43 @@ public class RbacJdbcManager {
     }
 
     public <T extends PersistableUser> List<T> findAllUsers(final Class<T> clazz) {
-//        final List<T> users = this.dsl.selectFrom(this.t_user).fetchInto(clazz);
-//        final Set<String> authorityValues = new HashSet<>();
-//        this.dsl.selectFrom(this.t_user_permission).
-        return null;
+        return this.crud.dsl().selectFrom(this.t_user).fetchInto(clazz);
+    }
+
+    public <T extends PersistableUser>
+    Optional<T> findUserByEmail(final Class<T> clazz, final String email) {
+        final T user = this.crud.dsl().selectFrom(this.t_user)
+            .where(DSL.field("email").eq(email))
+            .fetchOneInto(clazz);
+        final Optional<T> userOpt = Optional.ofNullable(user);
+        userOpt.ifPresent(u -> u.setPermissions(this.findPermissionByUser(u)));
+        return userOpt;
+    }
+
+    public <T extends PersistableUser>
+    Optional<T> findUserByMobile(final Class<T> clazz, final String mobile) {
+        final T user = this.crud.dsl().selectFrom(this.t_user)
+            .where(DSL.field("mobile").eq(mobile))
+            .fetchOneInto(clazz);
+        final Optional<T> userOpt = Optional.ofNullable(user);
+        userOpt.ifPresent(u -> u.setPermissions(this.findPermissionByUser(u)));
+        return userOpt;
     }
 
     public List<Permission> findPermissionByUser(final PersistableUser user) {
-//        this.dsl.select(
-//            this.t_permission.field("id"),
-//            this.t_permission.field("name"),
-//            this.t_permission.field("authority")
-//        ).from(this.t_permission, this.t_user_permission, this.t_role_permission)
-//            .where(this.t_user_permission.field("user_id").eq(user.getId()))
-
-//        this.dsl.selectFrom(this.t_user_permission)
-        return null;
-    }
-
-    public <T extends DefaultUserDetails> List<T> findUserByEmail(final Class<T> clazz) {
-        return null;//this.dsl.selectFrom(this.t_user).fetchInto(clazz);
+        return this.crud.dsl().select(PERMISSION_FIELDS)
+            .from(PERMISSION, USER_PERMISSION, ROLE_PERMISSION)
+            .where(USER_PERMISSION.USER_ID.eq(user.getId()).and(USER_PERMISSION.PERMISSION_ID.eq(PERMISSION.ID)))
+            .or(ROLE_PERMISSION.ROLE_ID.eq(user.getRoleId()).and(ROLE_PERMISSION.PERMISSION_ID.eq(PERMISSION.ID)))
+            .fetchInto(Permission.class);
     }
 
     public List<PermissionDetails> findAllPermissionDetails() {
-        return null;//this.dsl.selectFrom(PERMISSION_DETAILS).fetchInto(PermissionDetails.class);
+        return this.crud.dsl().selectFrom(PERMISSION_DETAILS).fetchInto(PermissionDetails.class);
     }
 
     public List<RoleDetails> findAllRoleDetails() {
-        return null;//this.dsl.selectFrom(ROLE_DETAILS).fetchInto(RoleDetails.class);
+        return this.crud.dsl().selectFrom(ROLE_DETAILS).fetchInto(RoleDetails.class);
     }
 
     public void createRole(final RoleDetails roleDetails) {
