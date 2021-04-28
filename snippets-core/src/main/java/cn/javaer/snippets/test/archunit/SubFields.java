8bcf264145a4e3042bd28a1e3fe5caecad81e290
cn-src <public@javaer.cn>
2021-04-28 11:07:28 +0800

    文档: 添加 javadoc

--- snippets-core/src/main/java/cn/javaer/snippets/test/archunit/SubFields.java
+++ snippets-core/src/main/java/cn/javaer/snippets/test/archunit/SubFields.java
@@ -7,6 +7,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 检测被注解 {@link SubFields} 的类的字段是否是 {@link SubFields#value()} 声明的类的字段的子集.
+ * <p>
+ * 判断时忽略静态字段和被 {@link org.springframework.data.annotation.Transient} 标注的字段.
+ * </p>
+ *
  * @author cn-src
  */
 @Documented
