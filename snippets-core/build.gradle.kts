cdeb5c63244d98315e233b2dab13577c5ddb1118
cn-src <public@javaer.cn>
2021-05-29 18:53:42 +0800

    依赖: 使用 kts 脚本

---
+++ snippets-core/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("snippets-conventions")
+}
+val commonsLang3Version: String by project
+val commonsCollections4Version: String by project
+val springdocVersion: String by project
+dependencies {
+    api("org.apache.commons:commons-lang3:$commonsLang3Version")
+    api("org.apache.commons:commons-collections4:$commonsCollections4Version")
+
+    optionalApi("com.tngtech.archunit:archunit-junit5")
+
+    compileOnly("com.fasterxml.jackson.core:jackson-annotations")
+    compileOnly("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
+
+    testImplementation(project(":snippets-jackson"))
+    testImplementation("com.h2database:h2")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
