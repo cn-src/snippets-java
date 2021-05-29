f12050547c1c38d0efbeb62f522084dbd36dc483
cn-src <public@javaer.cn>
2021-05-29 21:15:45 +0800

    依赖: 使用 kts 脚本

---
+++ snippets-spring-boot-starter-eclipse-collections/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("snippets-starter")
+}
+val eclipseCollectionsVersion: String by project
+val jacksonDatatypesCollectionsVersion: String by project
+dependencies {
+    api("org.eclipse.collections:eclipse-collections:${eclipseCollectionsVersion}")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-eclipse-collections:${jacksonDatatypesCollectionsVersion}")
+}
