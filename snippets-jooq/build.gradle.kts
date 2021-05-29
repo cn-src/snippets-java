1c2e2226abd8686d469a8443bad25a4be91a0bac
cn-src <public@javaer.cn>
2021-05-29 19:00:27 +0800

    依赖: 使用 kts 脚本

---
+++ snippets-jooq/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("snippets-conventions")
+}
+val jooqVersion: String by project
+dependencies {
+    api(project(":snippets-core"))
+    api(project(":snippets-jackson"))
+    api("org.jooq:jooq:$jooqVersion")
+
+    optionalApi("org.springframework:spring-beans")
+    optionalApi("org.postgresql:postgresql")
+
+    testImplementation("com.h2database:h2")
+    testImplementation("org.springframework.data:spring-data-jdbc")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.testcontainers:junit-jupiter")
+}
