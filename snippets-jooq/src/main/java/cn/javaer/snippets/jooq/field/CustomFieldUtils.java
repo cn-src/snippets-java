74a22cccdc5507cddd92ca410172b6330a48920c
cn-src <public@javaer.cn>
2021-06-03 17:23:39 +0800

    重构: CustomFieldUtils.java

--- snippets-jooq/src/main/java/cn/javaer/snippets/jooq/field/CustomFieldUtils.java
+++ snippets-jooq/src/main/java/cn/javaer/snippets/jooq/field/CustomFieldUtils.java
@@ -5,16 +5,27 @@ import org.jooq.Table;
 import org.jooq.impl.TableImpl;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author cn-src
  */
 interface CustomFieldUtils {
+    /**
+     * 添加字段到 jOOQ 默认字段表中.
+     *
+     * @param table table
+     * @param field field
+     */
     static void addToFields(final Table<?> table, final Field<?> field) {
         if (table instanceof TableImpl) {
             try {
-                final Object fields = TableImpl.class.getDeclaredMethod("fields0").invoke(table);
-                fields.getClass().getDeclaredMethod("add", Field.class).invoke(fields, field);
+                final Method fields0 = TableImpl.class.getDeclaredMethod("fields0");
+                fields0.setAccessible(true);
+                final Object fields = fields0.invoke(table);
+                final Method add = fields.getClass().getDeclaredMethod("add", Field.class);
+                add.setAccessible(true);
+                add.invoke(fields, field);
             }
             catch (final IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
                 // TODO
