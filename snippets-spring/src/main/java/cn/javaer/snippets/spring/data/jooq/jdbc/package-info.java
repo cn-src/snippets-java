c34f1a7b91a89e303c8c2e7af045c2d03fb11832
cn-src <public@javaer.cn>
2021-08-19 10:02:55 +0800

    重构清理

--- snippets-spring/src/main/java/cn/javaer/snippets/spring/data/jooq/jdbc/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * @author cn-src
- */
-@NonNullApi
-package cn.javaer.snippets.spring.data.jooq.jdbc;
-
-import org.springframework.lang.NonNullApi;
--- snippets-spring/src/test/java/cn/javaer/snippets/spring/data/jooq/jdbc/UserJdbcRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.javaer.snippets.spring.data.jooq.jdbc;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface UserJdbcRepository extends JooqJdbcRepository<User, Long> {
-
-}
