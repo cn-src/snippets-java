1d9ab53bba537a1a27ccd670546f1ce7a7fbcd31
cn-src <public@javaer.cn>
2021-08-20 18:31:26 +0800

    构建: releases 0.0.1.rc7

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "com.github.cn-src.snippets-java"
-    version = "0.0.1.rc3"
+    version = "0.0.1.rc7"
 
     repositories {
         mavenCentral()
