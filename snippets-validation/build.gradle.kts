8fbae5eb5dbe975c40a90193308c02d8144c8430
cn-src <public@javaer.cn>
2021-08-20 19:31:56 +0800

    构建: snippets-validation 依赖更新

--- snippets-validation/build.gradle.kts
+++ snippets-validation/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 val commonsLang3Version: String by project
 dependencies {
     api("org.apache.commons:commons-lang3:$commonsLang3Version")
-    api("org.springframework:spring-beans")
-    implementation("org.hibernate.validator:hibernate-validator:7.0.1.Final")
-    implementation("org.glassfish:jakarta.el:4.0.1")
+    optionalApi("org.springframework:spring-beans")
+    optionalApi("org.hibernate.validator:hibernate-validator:7.0.1.Final")
+    optionalApi("org.glassfish:jakarta.el:4.0.1")
 }
