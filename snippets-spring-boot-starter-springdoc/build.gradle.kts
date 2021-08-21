87520651936c53300845ec21975a25393accbbc9
cn-src <public@javaer.cn>
2021-08-21 14:57:59 +0800

    重构: 移动 starter

--- snippets-spring-boot-starter-springdoc/build.gradle.kts
+++ snippets-spring-boot-starter-springdoc/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
 val springdocVersion: String by project
 dependencies {
     api(project(":snippets-springdoc"))
+    api(project(":snippets-spring-boot-starter"))
     api("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
 
-    optionalApi("org.springframework.boot:spring-boot-starter")
+    optionalApi("org.springframework.boot:spring-boot-starter-data-jdbc")
+    testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
