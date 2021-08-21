21ae4682f22ba404f5a9eb2add92713d5d382d06
cn-src <public@javaer.cn>
2021-08-21 14:49:06 +0800

    重构: 移动 starter

--- snippets-spring-boot-starter-minio/build.gradle.kts
+++ snippets-spring-boot-starter-minio/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
-    id("snippets-starter")
+    id("snippets-conventions")
 }
+
 val minioVersion: String by project
 dependencies {
     api("io.minio:minio:$minioVersion")
-}
 
+    optionalApi("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
