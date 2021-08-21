8c79b1c4da5126be4f761f8a05859646d41e9946
cn-src <public@javaer.cn>
2021-08-21 14:59:57 +0800

    重构: 移动 starter

--- snippets-spring-boot-starter-p6spy/build.gradle.kts
+++ snippets-spring-boot-starter-p6spy/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("snippets-starter")
+    id("snippets-conventions")
 }
 val datasourceDecoratorVersion: String by project
 dependencies {
     api(project(":snippets-p6spy"))
     api("com.github.gavlyukovskiy:p6spy-spring-boot-starter:$datasourceDecoratorVersion")
-}
 
+    optionalApi("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
