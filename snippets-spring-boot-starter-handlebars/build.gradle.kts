507067c879b297f57a6e67b06e5f5da4ed98bbd8
cn-src <public@javaer.cn>
2021-08-21 14:53:19 +0800

    重构: 移动 starter

--- snippets-spring-boot-starter-handlebars/build.gradle.kts
+++ snippets-spring-boot-starter-handlebars/build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
 }
 val handlebarsVersion: String by project
 dependencies {
-    optionalApi("org.springframework.boot:spring-boot-starter")
     api("com.github.jknack:handlebars:$handlebarsVersion")
+
+    optionalApi("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- snippets-spring-boot-starter-springdoc/build.gradle.kts
+++ snippets-spring-boot-starter-springdoc/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
-    id("snippets-starter")
+    id("snippets-conventions")
 }
 val springdocVersion: String by project
 dependencies {
     api(project(":snippets-springdoc"))
     api("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
-}
 
+    optionalApi("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
