4299189ce8a385a0b3a2fa1b5606288939eac00f
cn-src <public@javaer.cn>
2021-08-23 22:39:10 +0800

    ci: github package 配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,19 +5,25 @@ subprojects {
     version = "0.0.1-SNAPSHOT"
 
     repositories {
-        repositories {
-            maven { url = uri("https://maven.aliyun.com/repository/public/") }
-            mavenLocal()
-            mavenCentral()
-        }
+//        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        mavenLocal()
+        mavenCentral()
     }
     configure<PublishingExtension> {
         repositories {
             maven {
-                val releasesRepoUrl = uri("$buildDir/repos/releases")
-                val snapshotsRepoUrl = uri("$buildDir/repos/snapshots")
-                val isSnapshot = (version as String).endsWith("SNAPSHOT")
-                url = if (isSnapshot) snapshotsRepoUrl else releasesRepoUrl
+                url = uri("https://maven.pkg.github.com/cn-src/snippets-java")
+                credentials {
+                    username = project.findProperty("gpr.user") as String?
+                        ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String?
+                        ?: System.getenv("TOKEN")
+                }
+            }
+        }
+        publications {
+            register<MavenPublication>("gpr") {
+                from(components["java"])
             }
         }
     }
