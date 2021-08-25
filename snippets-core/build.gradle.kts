1da05d3ad4f8a083e4ef14aa3a62b3fd90268883
cn-src <public@javaer.cn>
2021-08-25 10:44:29 +0800

    测试: 清理测试

--- snippets-core/build.gradle.kts
+++ snippets-core/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     api("cn.hutool:hutool-core:$hutoolVersion")
 
     optionalApi("org.springframework.data:spring-data-commons")
-    optionalImplementation("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
+    optionalApi("org.springdoc:springdoc-openapi-webmvc-core:$springdocVersion")
     compileOnly("com.fasterxml.jackson.core:jackson-annotations")
 
     testImplementation(project(":snippets-jackson"))
