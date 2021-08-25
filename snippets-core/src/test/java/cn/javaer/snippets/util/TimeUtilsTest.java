2bdb5ed7405e4146b6aa9df0f8b65de98495ed11
cn-src <public@javaer.cn>
2021-08-25 11:36:51 +0800

    重构: 重构工具类

---
+++ snippets-core/src/test/java/cn/javaer/snippets/util/TimeUtilsTest.java
@@ -0,0 +1,27 @@
+package cn.javaer.snippets.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author cn-src
+ */
+class TimeUtilsTest {
+
+    @Test
+    void monthStart() {
+        final LocalDateTime dateTime = TimeUtils.monthStart();
+        assertThat(dateTime.getDayOfMonth()).isEqualTo(1);
+        assertThat(dateTime.toLocalTime()).isEqualTo(LocalTime.MIN);
+    }
+
+    @Test
+    void monthEnd() {
+        final LocalDateTime dateTime = TimeUtils.monthEnd();
+        assertThat(dateTime.toLocalTime()).isEqualTo(LocalTime.MAX);
+    }
+}
